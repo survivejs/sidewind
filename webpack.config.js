066c959ec7344182df3873c96f3a7dbc1e591c57
Juho Vepsäläinen <bebraw@gmail.com>
2019-12-30 14:07:53 +0100

    chore(webpack): Add missing meta tags

--- webpack.config.js
+++ webpack.config.js
@@ -219,6 +219,8 @@ function getHTML({
   <html${htmlAttrs}>
     <head>
       <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
       <title>${title}</title>
       ${cssTags}
     </head>
