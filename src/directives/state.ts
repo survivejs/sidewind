743a60482288047bf19aaba5d5160b965709fd89
Juho Vepsäläinen <bebraw@gmail.com>
2020-01-09 11:19:50 +0100

    refactor(state): Drop container sorting
    Since the algorithm is different now, no sorting is required.

--- src/directives/state.ts
+++ src/directives/state.ts
@@ -4,34 +4,13 @@ import evaluateClasses from "./classes";
 import evaluateValues from "./values";
 
 function evaluateState(stateContainers: NodeListOf<ExtendedHTMLElement>) {
-  // It's important to perform state initialization parent-first since
-  // state is nested and shadowed by children.
-  const stateContainerOrder = orderByParents(Array.from(stateContainers));
-
-  stateContainerOrder.forEach(i => {
+  for (let i = stateContainers.length; i--; ) {
     const stateContainer = stateContainers[i];
     const state = parseState(stateContainer);
 
     evaluateValues(stateContainer, state, "x-value", "x-state");
     evaluateClasses(stateContainer, state);
-  });
-}
-
-function orderByParents(elementsArray: ExtendedHTMLElement[]) {
-  // Note that sort mutates the original structure directly
-  return elementsArray
-    .map((element, i) => ({
-      i,
-      depth: getDepth(element),
-    }))
-    .sort((a, b) => a.depth - b.depth)
-    .map(({ i }) => i);
-}
-
-function getDepth(element: Element, depth = 0): number {
-  return element.parentElement
-    ? getDepth(element.parentElement, depth + 1)
-    : depth;
+  }
 }
 
 export default evaluateState;
