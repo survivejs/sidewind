9cdc89e078c7759ee7728ce2d6a5bf300e212235
Juho Vepsäläinen <bebraw@gmail.com>
2020-01-14 21:28:46 +0100

    fix(x-each): Evaluate x-each correctly on initialization
    Likely this can be simplified.

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,23 @@ declare global {
 
 function initialize(global = window) {
   global.onload = () => {
-    evaluateEach(document.querySelectorAll("[x-each]"), "x-each", "x-state");
     evaluateClosest(
       document.querySelectorAll("[x-closest]"),
       "x-closest",
       "x-state"
     );
+    evaluateState(
+      document.querySelectorAll("[x-state]"),
+      "x-state",
+      "x-bind",
+      "x-each",
+      "x-attr",
+      "x-label"
+    );
+
+    // Since x-each generates x-state, re-evaluation is needed
+    // TODO: Figure out a way to simplify
+    evaluateEach(document.querySelectorAll("[x-each]"), "x-each", "x-state");
     evaluateState(
       document.querySelectorAll("[x-state]"),
       "x-state",
