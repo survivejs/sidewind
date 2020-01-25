38ebe8ae57eabdd754d70d12c46bb6b6b51c6264
Juho Vepsäläinen <bebraw@gmail.com>
2020-01-25 21:29:24 +0100

    refactor: Extract directive initialization to client side

--- demo/index.ts
+++ demo/index.ts
@@ -1,6 +1,39 @@
-import { initialize } from "../src";
+import { evaluateDirective, setState } from "../src";
+import {
+  attributesDirective,
+  closestDirective,
+  eachDirective,
+  intervalDirective,
+  intersectDirective,
+  stateDirective,
+  valueDirective,
+} from "../src/directives";
 
 import "highlight.js/styles/dracula.css";
 import "./index.pcss";
+
+declare global {
+  interface Window {
+    setState: typeof setState;
+  }
+}
+
+function evaluateDirectives() {
+  evaluateDirective("x-closest", closestDirective);
+  evaluateDirective("x-interval", intervalDirective);
+  evaluateDirective("x-intersect", intersectDirective);
+  evaluateDirective("x-state", stateDirective);
+  evaluateDirective("x-each", eachDirective);
+  evaluateDirective("x-attr", attributesDirective);
+  evaluateDirective("x", valueDirective);
+}
+
+function initialize(global = window) {
+  global.onload = evaluateDirectives;
+
+  // TODO: Pass evaluateDirectives + figure out how setState would
+  // work internally (avoid recursion)
+  global.setState = setState;
+}
 
 initialize();
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,4 @@
-import {
-  attributesDirective,
-  closestDirective,
-  eachDirective,
-  intervalDirective,
-  intersectDirective,
-  stateDirective,
-  valueDirective,
-} from "./directives";
 import setState from "./set-state";
 import evaluateDirective from "./evaluate-directive";
 
-declare global {
-  interface Window {
-    setState: typeof setState;
-  }
-}
-
-function evaluateDirectives() {
-  evaluateDirective("x-closest", closestDirective);
-  evaluateDirective("x-interval", intervalDirective);
-  evaluateDirective("x-intersect", intersectDirective);
-  evaluateDirective("x-state", stateDirective);
-  evaluateDirective("x-each", eachDirective);
-  evaluateDirective("x-attr", attributesDirective);
-  evaluateDirective("x", valueDirective);
-}
-
-function initialize(global = window) {
-  global.onload = evaluateDirectives;
-
-  // TODO: Pass evaluateDirectives + figure out how setState would
-  // work internally (avoid recursion)
-  global.setState = setState;
-}
-
-export { initialize };
+export { evaluateDirective, setState };
