b1e532e0628ec969630cdb08514c72653594e867
Juho Vepsäläinen <bebraw@gmail.com>
2020-01-26 15:33:49 +0100

    chore(x-each): Escape early

--- src/directives/each.ts
+++ src/directives/each.ts
@@ -8,40 +8,35 @@ function eachDirective({
   setState,
 }: DirectiveParameters) {
   const { state } = getState(element);
+  const containerParent = element.parentNode;
 
-  if (state) {
-    const containerParent = element.parentNode;
-
-    if (!containerParent) {
-      return;
-    }
-
-    if (typeof state === "object") {
-      // It would be better to diff for changes instead of replacing
-      // all nodes.
-      while (containerParent.firstChild) {
-        containerParent.firstChild.remove();
-      }
-
-      Object.values(getValues(state, expression)).forEach(
-        values =>
-          Array.isArray(values) &&
-          values.forEach((value: any) => {
-            const templateClone = document.importNode(element.content, true);
-            const firstChild = templateClone.firstElementChild;
-
-            // The element should be a state container itself
-            firstChild.setAttribute("x-state", "");
-
-            containerParent.appendChild(templateClone);
-            setState(value, firstChild);
-          })
-      );
-
-      // Append last to trigger mutation observer only once
-      containerParent.appendChild(element);
-    }
+  if (!state || !containerParent || typeof state !== "object") {
+    return;
   }
+
+  // It would be better to diff for changes instead of replacing
+  // all nodes.
+  while (containerParent.firstChild) {
+    containerParent.firstChild.remove();
+  }
+
+  Object.values(getValues(state, expression)).forEach(
+    values =>
+      Array.isArray(values) &&
+      values.forEach((value: any) => {
+        const templateClone = document.importNode(element.content, true);
+        const firstChild = templateClone.firstElementChild;
+
+        // The element should be a state container itself
+        firstChild.setAttribute("x-state", "");
+
+        containerParent.appendChild(templateClone);
+        setState(value, firstChild);
+      })
+  );
+
+  // Append last to trigger mutation observer only once
+  containerParent.appendChild(element);
 }
 
 export default eachDirective;
