0836c8a19883b679d3a31f9510af936da6223508
Juho Vepsäläinen <bebraw@gmail.com>
2020-01-26 18:08:30 +0100

    chore: Simplify bootstrap script

--- demo/index.ts
+++ demo/index.ts
@@ -19,19 +19,15 @@ declare global {
   }
 }
 
-function initialize(global = window) {
-  global.onload = () =>
-    evaluateDirectives([
-      { name: "x-state", directive: stateDirective },
-      { name: "x-each", directive: eachDirective },
-      { name: "x-attr", directive: attributesDirective },
-      { name: "x", directive: valueDirective },
-      { name: "x-closest", directive: closestDirective },
-      { name: "x-interval", directive: intervalDirective },
-      { name: "x-intersect", directive: intersectDirective },
-    ]);
+window.onload = () =>
+  evaluateDirectives([
+    { name: "x-state", directive: stateDirective },
+    { name: "x-each", directive: eachDirective },
+    { name: "x-attr", directive: attributesDirective },
+    { name: "x", directive: valueDirective },
+    { name: "x-closest", directive: closestDirective },
+    { name: "x-interval", directive: intervalDirective },
+    { name: "x-intersect", directive: intersectDirective },
+  ]);
 
-  global.setState = setState;
-}
-
-initialize();
+window.setState = setState;
