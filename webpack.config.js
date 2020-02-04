fe8647882e80849adc71cfaf5093dcbc91e9d9e2
Juho Vepsäläinen <bebraw@gmail.com>
2020-02-04 17:14:50 +0100

    fix: Fix code editor in Safari
    I don't like the extra margin/padding but looks like Safari needs that
    as there's some difference in rendering and width calculation for
    overflow.
    
    Closes #11.

--- webpack.config.js
+++ webpack.config.js
@@ -144,9 +144,9 @@ function expandCode() {
           return `<section class="mb-4" x-state="{ code: atob('${decodedExample}') }">
     <div class="p-4 bg-gray-800 text-white rounded-t-lg overflow-x-auto overflow-y-hidden">
       <div class="inline-block font-mono relative">
-        <pre class="overflow-auto" x="highlight('html', state.code)"></pre>
+        <pre class="overflow-hidden mr-16 pr-16 w-full" x="highlight('html', state.code)"></pre>
         <textarea
-          class="absolute min-w-full top-0 left-0 outline-none opacity-50 bg-none whitespace-pre"
+          class="overflow-hidden absolute min-w-full top-0 left-0 outline-none opacity-50 bg-none whitespace-pre"
           oninput="setState({ code: this.value })"
           x="state.code"
           autocapitalize="off"
