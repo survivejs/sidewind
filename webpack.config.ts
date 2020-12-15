cfce652d5f0753a967aa0df932ca06d9272c0bc1
Juho Vepsäläinen <bebraw@gmail.com>
2020-12-15 16:27:55 +0100

    refactor: Port configuration to TS + drop Tailwind
    Tailwind will be replaced with something else.

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,21 +1,26 @@
-const fs = require("fs");
-const path = require("path");
-const temp = require("temp");
-const glob = require("glob");
-const showdown = require("showdown");
-const decodeHTML = require("html-encoder-decoder").decode;
-const { mode } = require("webpack-nano/argv");
-const { WebpackPluginServe } = require("webpack-plugin-serve");
-const { merge } = require("webpack-merge");
-const CopyPlugin = require("copy-webpack-plugin");
-const {
+import webpack from "webpack";
+import type {
+  Configuration,
+  Compilation,
+  WebpackPluginInstance,
+} from "webpack";
+import fs from "fs";
+import path from "path";
+import temp from "temp";
+import showdown from "showdown";
+import { merge } from "webpack-merge";
+import { WebpackPluginServe } from "webpack-plugin-serve";
+import CopyPlugin from "copy-webpack-plugin";
+import {
   MiniHtmlWebpackPlugin,
   generateAttributes,
   generateCSSReferences,
   generateJSReferences,
-} = require("mini-html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const PurgeCSSPlugin = require("purgecss-webpack-plugin");
+} from "mini-html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+const decodeHTML = require("html-encoder-decoder").decode;
+const { mode } = require("webpack-nano/argv");
 
 const PATHS = {
   ASSETS: path.resolve(__dirname, "assets"),
@@ -42,7 +47,7 @@ const commonConfig = merge({
       },
       {
         test: /\.(css|pcss)$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
   },
@@ -75,13 +80,13 @@ const commonConfig = merge({
         const htmlAttrs = generateAttributes(htmlAttributes);
 
         const cssTags = generateCSSReferences({
-          files: css,
+          files: css || [],
           attributes: cssAttributes,
           publicPath,
         });
 
         const jsTags = generateJSReferences({
-          files: js,
+          files: js || [],
           attributes: jsAttributes,
           publicPath,
         });
@@ -92,11 +97,12 @@ const commonConfig = merge({
   ],
 });
 
-function processMarkdown(input) {
+function processMarkdown(input: string) {
   const classMap = { a: "underline", ul: "list-disc list-inside" };
-  const bindings = Object.keys(classMap).map((key) => ({
+  const bindings = Object.keys(classMap).map((key: string) => ({
     type: "output",
     regex: new RegExp(`<${key}(.*)>`, "g"),
+    // @ts-ignore: No index signature with a parameter of type 'string' was found
     replace: `<${key} class="${classMap[key]}" $1>`,
   }));
   const convert = new showdown.Converter({
@@ -123,7 +129,7 @@ function styleH1() {
   };
 }
 
-function addHeadingAnchors(tag) {
+function addHeadingAnchors(tag: string) {
   return {
     type: "output",
     regex: new RegExp(`<${tag} id="(.*)">(.*)<\/${tag}>`, "g"),
@@ -134,11 +140,11 @@ function addHeadingAnchors(tag) {
 function expandCode() {
   return {
     type: "output",
-    filter(text) {
+    filter(text: string) {
       const left = "<pre><code\\b[^>]*>",
         right = "</code></pre>",
         flags = "g",
-        replacement = (_, match) => {
+        replacement = (_: any, match: string) => {
           const example = decodeHTML(match).trim();
           const decodedExample = Buffer.from(example).toString("base64");
 
@@ -172,13 +178,19 @@ function expandCode() {
   };
 }
 
-class AddDependencyPlugin {
-  constructor(options = {}) {
+type Options = {
+  path: string;
+};
+
+class AddDependencyPlugin implements WebpackPluginInstance {
+  private options: Options;
+
+  constructor(options: Options) {
     this.options = options;
     this.plugin = this.plugin.bind(this);
   }
 
-  plugin(compilation, callback) {
+  plugin(compilation: Compilation, callback: () => void) {
     const { path } = this.options;
 
     compilation.fileDependencies.add(path);
@@ -186,20 +198,20 @@ class AddDependencyPlugin {
     callback();
   }
 
-  apply(compiler) {
+  apply(compiler: webpack.Compiler) {
     compiler.hooks.emit.tapAsync("AddDependencyPlugin", this.plugin);
   }
 }
 
-const getConfig = (mode) => {
+const getConfig = (mode: Configuration["mode"]): Configuration => {
   switch (mode) {
     case "development": {
-      return merge(commonConfig, {
+      return merge<Configuration>(commonConfig, {
         mode,
         watch: true,
         plugins: [
           new WebpackPluginServe({
-            port: process.env.PORT || 8080,
+            port: parseInt(process.env.PORT || "") || 8080,
             static: PATHS.OUTPUT,
             liveReload: true,
             waitForBuild: true,
@@ -215,24 +227,11 @@ const getConfig = (mode) => {
 
       fs.writeFileSync(path.join(htmlDir, "index.html"), getHTML());
 
-      return merge(commonConfig, {
-        mode,
-        plugins: [
-          new PurgeCSSPlugin({
-            whitelistPatterns: [/^hljs/, /^space-x/, /^space-y/], // Example: /^svg-/
-            paths: glob.sync(`${htmlDir}/**/*.html`),
-            extractors: [
-              {
-                extractor: (content) =>
-                  content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [],
-                extensions: ["html"],
-              },
-            ],
-          }),
-        ],
-      });
+      return merge<Configuration>(commonConfig, { mode });
     }
   }
+
+  throw new Error("Matching configuration was not found!");
 };
 
 module.exports = getConfig(mode);
@@ -296,6 +295,6 @@ function getHTML({
 }
 
 // http://tholman.com/github-corners/
-function githubCorner(url) {
+function githubCorner(url: string) {
   return `<a href="${url}" class="github-corner fixed top-0 right-0" aria-label="View source on GitHub"><svg width="80" height="80" viewBox="0 0 250 250" style="fill:#151513; color:#fff; position: absolute; top: 0; border: 0; right: 0;" aria-hidden="true"><path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path><path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path><path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path></svg></a><style>.github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}</style>`;
 }
