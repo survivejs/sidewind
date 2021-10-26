d2815dae7d529fbceecdfa54e1f25505193912c6
Juho Vepsäläinen <bebraw@gmail.com>
2021-10-26 17:49:47 +0300

    fix: Allow x-each to work against indirect data changes

--- src/directives/each.ts
+++ src/directives/each.ts
@@ -1,5 +1,5 @@
 import getParents from "../get-parents";
-import type { DirectiveParameters } from "../types";
+import type { DirectiveParameters, ExtendedHTMLElement } from "../types";
 
 function eachDirective({
   element,
@@ -16,33 +16,51 @@ function eachDirective({
     return;
   }
 
-  if (containerParent.children.length > 1) {
-    return;
-  }
+  const initializedAlready = containerParent.children.length > 1;
 
   // Mark container parent as a boundary for x-recurse to copy
   containerParent.setAttribute("_x", "");
 
   const level = getParents(element, "x-recurse").length;
 
-  state.forEach((value: any) => {
-    const templateClone = document.importNode(element.content, true);
-    const firstChild = templateClone.firstElementChild;
-    containerParent.appendChild(templateClone);
+  // If we had initialized already, then it's important to update state
+  // per each item
+  if (initializedAlready) {
+    let child = containerParent.firstElementChild
+      ?.nextElementSibling as ExtendedHTMLElement;
 
-    let child = firstChild;
-
-    do {
+    state.forEach((value: any) => {
       if (child) {
-        // The element should be a state container itself
-        child.setAttribute("x-state", "");
         // The actual state is stored to the object
         child.state = { value, level };
 
         evaluateDirectives(directives, child);
+
+        child = child.nextElementSibling as ExtendedHTMLElement;
       }
-    } while ((child = child?.nextElementSibling));
-  });
+    });
+    // Otherwise we'll set up the initial DOM structure based on a template
+    // and populate it with data that's then evaluated.
+  } else {
+    state.forEach((value: any) => {
+      const templateClone = document.importNode(element.content, true);
+      const firstChild = templateClone.firstElementChild;
+      containerParent.appendChild(templateClone);
+
+      let child = firstChild;
+
+      do {
+        if (child) {
+          // The element should be a state container itself
+          child.setAttribute("x-state", "");
+          // The actual state is stored to the object
+          child.state = { value, level };
+
+          evaluateDirectives(directives, child);
+        }
+      } while ((child = child?.nextElementSibling));
+    });
+  }
 }
 
 export default eachDirective;
