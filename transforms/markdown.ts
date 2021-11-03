0ee972d6bd8519d798a9b1dc2ccd00ee96bf5c85
Juho Vepsäläinen <bebraw@gmail.com>
2021-11-03 16:53:09 +0200

    chore: Update gustwind

--- transforms/markdown.ts
+++ transforms/markdown.ts
@@ -1,4 +1,4 @@
 // Import maps don't work inside import()
-import { transforms } from "https://deno.land/x/gustwind@v0.4.7/mod.ts";
+import { transforms } from "https://deno.land/x/gustwind@v0.4.8/mod.ts";
 
 export default transforms.markdown;
