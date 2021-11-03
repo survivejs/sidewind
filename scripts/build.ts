9cd096c7f4b32ee55bd49d06fecfd64a3ca6cb4e
Juho Vepsäläinen <bebraw@gmail.com>
2021-11-03 16:47:26 +0200

    fix: Fix transforms

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,12 +1,12 @@
 import * as path from "path";
-import { serve } from "gustwind";
+import { build } from "gustwind";
 import { getJson } from "./utils.ts";
 
 async function main() {
   const siteMeta = await getJson("./meta.json");
   const projectRoot = path.join(path.fromFileUrl(import.meta.url), "..", "..");
 
-  serve({ ...siteMeta, projectRoot });
+  build({ ...siteMeta, projectRoot });
 }
 
 main();
--- transforms/markdown.ts
+++ transforms/markdown.ts
@@ -1,7 +1,4 @@
-/*
-import { transforms } from "gustwind";
+// Import maps don't work inside import()
+import { transforms } from "https://deno.land/x/gustwind@v0.4.7/mod.ts";
 
 export default transforms.markdown;
-*/
-
-export default (input) => input;
