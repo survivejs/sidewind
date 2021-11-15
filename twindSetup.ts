3cb7a1c3e5e4f46d7ceefa994a28b07d980e34fc
Juho Vepsäläinen <bebraw@gmail.com>
2021-11-15 13:00:59 +0200

    chore: Move typography before other rules

--- twindSetup.ts
+++ twindSetup.ts
@@ -4,9 +4,9 @@ import twindTypography from "https://cdn.skypack.dev/@twind/typography@0.0.2?min
 export default {
   theme: { extend: { colors: twindColors } },
   plugins: {
+    ...twindTypography(),
     btn: `font-bold py-2 px-4 rounded`,
     "btn-blue": `bg-blue-500 hover:bg-blue-700 text-white`,
     "btn-muted": `font-light text-gray-500`,
-    ...twindTypography(),
   },
 };
