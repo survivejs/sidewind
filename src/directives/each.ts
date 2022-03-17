0cee673ad8196333cf1ab2016b09568dbe9b92a3
Juho Vepsäläinen <bebraw@gmail.com>
2022-03-17 16:28:34 +0200

    chore(x-each): Sketch out update logic

--- src/directives/each.ts
+++ src/directives/each.ts
@@ -26,72 +26,67 @@ function eachDirective({
     return;
   }
 
-  const hasParentEach = !!element.closest("[x-has-each]");
+  if (element.templates?.length > 0) {
+    const amountOfItems = state.length;
+    const amountOfChildren = element.children.length;
 
-  // Stash state so it can be compared later to avoid work.
-  // In case state is derived from another x-each, use getState(element) here.
-  element.state = hasParentEach ? elementState.state : state;
+    console.log("create missing nodes", state, amountOfItems, amountOfChildren);
 
-  let templates = element.querySelectorAll(":scope > *[x-template]");
-
-  if (!templates.length) {
-    console.error("x-each - x-template was not found", element);
-
-    return;
-  }
-
-  // If x-template isn't meant to be a group, pick only the first one
-  if (templates[0].getAttribute("x-template") !== "group") {
-    templates = [templates[0]];
-  }
-
-  // Stash template for future use
-  element.templates = templates;
-
-  // Mark element as initialized (needed by state evaluation)
-  element.setAttribute("_x-init", 1);
-
-  // Empty contents as they'll be replaced by applying the template
-  while (element.firstChild) {
-    element.removeChild(element.lastChild);
-  }
-
-  const level =
-    getParents(element, "x-recurse").length +
-    (element.hasAttribute("x-recurse") ? 1 : 0);
-
-  // Apply the templates against each item in the collection
-  state.forEach((value: any) => {
-    for (let i = 0; i < templates.length; i++) {
-      const template = templates[i];
-      const templateClone = template.cloneNode(true);
-
-      // Remote template mark
-      templateClone.removeAttribute("x-template");
-
-      // The element should be a state container itself
-      templateClone.setAttribute("x-state", "");
-
-      // Mark as a former template so that recursion (x-recurse) can find it
-      templateClone.setAttribute("_x-template", "");
-
-      // The actual state is stored to the object
-      templateClone.state = { value, level };
-
-      element.appendChild(templateClone);
-      element.setAttribute("x-has-each", "");
-
-      evaluateDirectives(directives, templateClone);
+    // Create missing nodes
+    if (amountOfItems > amountOfChildren) {
+      for (let i = 0; i < amountOfItems - amountOfChildren; i++) {
+        // TODO: Copy enough template(s) here
+        // const templateClone = element.cloneNode(true);
+        // element.appendChild(templateClone.firstElementChild);
+      }
     }
-  });
+    // Remove extra nodes
+    if (amountOfItems < amountOfChildren) {
+      for (let i = 0; i < amountOfChildren - amountOfItems; i++) {
+        element.lastElementChild?.remove();
+      }
+    }
 
-  // Remove possible initial nodes (SSR)
-  /*
-  containerParent
-    .querySelectorAll("*[x-initial='']")
-    // @ts-ignore Skip this check
-    .forEach((e: ExtendedHTMLElement) => e.remove());
-  */
+    // TODO: Update each template
+  } else {
+    const hasParentEach = !!element.closest("[x-has-each]");
+
+    // Stash state so it can be compared later to avoid work.
+    // In case state is derived from another x-each, use getState(element) here.
+    element.state = hasParentEach ? elementState.state : state;
+
+    let templates = element.querySelectorAll(":scope > *[x-template]");
+
+    if (!templates.length) {
+      console.error("x-each - x-template was not found", element);
+
+      return;
+    }
+
+    // If x-template isn't meant to be a group, pick only the first one
+    if (templates[0].getAttribute("x-template") !== "group") {
+      templates = [templates[0]];
+    }
+
+    // Stash template for future use
+    element.templates = templates;
+
+    // Mark element as initialized (needed by state evaluation)
+    element.setAttribute("_x-init", 1);
+
+    // Empty contents as they'll be replaced by applying the template
+    while (element.firstChild) {
+      element.removeChild(element.lastChild);
+    }
+
+    const level =
+      getParents(element, "x-recurse").length +
+      (element.hasAttribute("x-recurse") ? 1 : 0);
+    const renderTemplate = getTemplateRenderer(element, templates, level);
+
+    state.forEach(renderTemplate);
+    evaluateDirectives(directives, element);
+  }
 
   /*
   const level = getParents(element, "x-recurse").length;
@@ -142,32 +137,38 @@ function eachDirective({
     if (child && state.length === 0) {
       child.remove();
     }
-    // Otherwise we'll set up the initial DOM structure based on a template
-    // and populate it with data that's then evaluated.
-  } else {
-    state.forEach((value: any) => {
-      const templateClone = document.importNode(element.content, true);
-      const firstChild = templateClone.firstElementChild;
-      containerParent.appendChild(templateClone);
-      containerParent.setAttribute("x-has-each", "");
-
-      let child = firstChild;
-
-      do {
-        if (child) {
-          // The element should be a state container itself
-          child.setAttribute("x-state", "");
-          // The actual state is stored to the object
-          child.state = { value, level };
-
-          evaluateDirectives(directives, child);
-        }
-      } while ((child = child?.nextElementSibling));
-    });
   }
   */
 }
 eachDirective.evaluateFrom = "top";
+
+function getTemplateRenderer(
+  element: ExtendedHTMLElement,
+  templates: NodeList,
+  level: number
+) {
+  return (value: any) => {
+    for (let i = 0; i < templates.length; i++) {
+      const template = templates[i];
+      const templateClone = template.cloneNode(true);
+
+      // Remote template mark
+      templateClone.removeAttribute("x-template");
+
+      // The element should be a state container itself
+      templateClone.setAttribute("x-state", "");
+
+      // Mark as a former template so that recursion (x-recurse) can find it
+      templateClone.setAttribute("_x-template", "");
+
+      // The actual state is stored to the object
+      templateClone.state = { value, level };
+
+      element.appendChild(templateClone);
+      element.setAttribute("x-has-each", "");
+    }
+  };
+}
 
 function findFirstChildrenWith(element: Element, tagName: string) {
   let ret: ExtendedHTMLElement[] = [];
