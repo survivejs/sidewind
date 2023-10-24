7ec22747b49d4b563d62b4ca37dae67d38c656bf
Juho Vepsäläinen <bebraw@gmail.com>
2023-10-24 13:01:18 +0300

    fix: Fix editor

--- site/twindSetup.ts
+++ site/twindSetup.ts
@@ -1,28 +1,11 @@
 import presetAutoprefix from "https://esm.sh/@twind/preset-autoprefix@1.0.5";
 import presetTailwind from "https://esm.sh/@twind/preset-tailwind@1.1.1";
-import presetTypography from "https://esm.sh/@twind/preset-typography@1.0.5";
 
 export default {
-  // mode: "silent",
-  // theme: { extend: { colors: twindColors } },
-  presets: [presetAutoprefix(), presetTailwind(), presetTypography()],
+  presets: [presetAutoprefix(), presetTailwind()],
   plugins: {
     btn: `font-bold py-2 px-4 rounded`,
     "btn-blue": `bg-blue-500 hover:bg-blue-700 text-white`,
     "btn-muted": `font-light text-gray-500`,
   },
 };
-
-/*
-import * as twindColors from "https://cdn.skypack.dev/twind@0.16.16/colors?min";
-
-export default {
-  mode: "silent",
-  theme: { extend: { colors: twindColors } },
-  plugins: {
-    btn: `font-bold py-2 px-4 rounded`,
-    "btn-blue": `bg-blue-500 hover:bg-blue-700 text-white`,
-    "btn-muted": `font-light text-gray-500`,
-  },
-};
-*/
