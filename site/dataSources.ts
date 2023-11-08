4a156c16a625775d0db73a32825c2fe9507edecc
Juho Vepsäläinen <bebraw@gmail.com>
2023-11-08 11:56:18 +0200

    fix: Parse documentation pages

--- site/dataSources.ts
+++ site/dataSources.ts
@@ -1,6 +1,17 @@
-import { indexMarkdown } from "https://deno.land/x/gustwind@v0.39.11/site/dataSources.ts";
+import { parse } from "https://deno.land/x/frontmatter@v0.1.4/mod.ts";
+import { dir } from "https://deno.land/x/gustwind@v0.39.11/utilities/fs.ts";
 import markdown from "./transforms/markdown.ts";
 import type { LoadApi } from "https://deno.land/x/gustwind@v0.52.3/types.ts";
+
+type MarkdownWithFrontmatter = {
+  data: {
+    slug: string;
+    title: string;
+    date: Date;
+    keywords: string[];
+  };
+  content: string;
+};
 
 function init({ load }: { load: LoadApi }) {
   async function processMarkdown(
@@ -18,6 +29,31 @@ function init({ load }: { load: LoadApi }) {
     return markdown(
       o?.skipFirstLine ? lines.split("\n").slice(1).join("\n") : lines
     );
+  }
+
+  async function indexMarkdown(
+    directory: string,
+    o?: { skipFirstLine: boolean }
+  ) {
+    const files = await dir({ path: directory, extension: ".md" });
+
+    return Promise.all(
+      files.map(({ path }) =>
+        Deno.readTextFile(path).then((d) => parseHeadmatter(d, o))
+      )
+    );
+  }
+
+  function parseHeadmatter(
+    s: string,
+    o?: { skipFirstLine: boolean }
+  ): MarkdownWithFrontmatter {
+    const ret = parse(s);
+
+    return {
+      ...ret,
+      content: parseMarkdown(ret.content, o).content,
+    } as MarkdownWithFrontmatter;
   }
 
   return { indexMarkdown, processMarkdown };
