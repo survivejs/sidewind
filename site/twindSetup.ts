e3a556b093af9c7aa9e25c2bfceed4e3d2ebd6db
Juho Vepsäläinen <bebraw@gmail.com>
2024-01-23 16:27:49 +0200

    fix: Fix button styles

--- site/twindSetup.ts
+++ site/twindSetup.ts
@@ -4,11 +4,11 @@ import presetTypography from "https://esm.sh/@twind/preset-typography@1.0.5";
 
 export default {
   presets: [presetAutoprefix(), presetTailwind(), presetTypography()],
-  plugins: {
-    btn: `font-bold py-2 px-4 rounded`,
-    "btn-blue": `bg-blue-500 hover:bg-blue-700 text-white`,
-    "btn-muted": `font-light text-gray-500`,
-  },
+  rules: [
+    ["btn", "font-bold py-2 px-4 rounded"],
+    ["btn-blue", "bg-blue-500 hover:bg-blue-700 text-white"],
+    ["btn-muted", "font-light text-gray-500"],
+  ],
   // Avoid hashing class names so it's easier to debug
   hash: false,
 };
